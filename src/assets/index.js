14b2561cdd072dc81b126f6652b3d991310f90c9
Saugat Rimal <saugatrimal60@gmail.com>
2023-02-12 00:12:49 +0545

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export { default as cubecss } from "./cubecss.svg";
+export { default as designtoken } from "./designtoken.svg";
+export { default as utopia } from "./utopia.svg";
+export { default as everylayout } from "./everylayout.svg";
+export { default as polypane } from "./polypane.svg";
+export { default as tailwind } from "./tailwind.svg";
+
+
